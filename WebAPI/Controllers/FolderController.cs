d93077b6ba1e1e1f22992d058f4c692e63b68f9e
VKabanov <vkabanov1978@gmail.com>
2016-08-22 16:31:13 +0300

    Init

---
+++ WebAPI/Controllers/FolderController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class FolderController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<DriveInfo> GetDrives()
+        {
+            List<DriveInfo> HDDList = new List<DriveInfo>();
+            CheckHDListForReadiness(ref HDDList);           
+          
+            return HDDList;
+        }
+
+        [HttpPost]
+        [Route("api/Folder/DownloadFile")]
+        public HttpResponseMessage DownloadFile([FromBody] string filePath)
+        {
+            try
+            {
+                HttpResponseMessage httpResponseMessage = new HttpResponseMessage();
+                if (!string.IsNullOrEmpty(filePath) &(System.IO.File.Exists(filePath)))
+                {
+                  using (MemoryStream stream = new MemoryStream())
+                    {
+                        using (FileStream file = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                        {
+                            byte[] bytes = new byte[file.Length];
+
+                            file.Read(bytes, 0, (int)file.Length);
+                            stream.Write(bytes, 0, (int)file.Length);
+
+                            httpResponseMessage.Content = new ByteArrayContent(bytes.ToArray());
+                            httpResponseMessage.Content.Headers.Add("x-filename", filePath);
+                            httpResponseMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                            httpResponseMessage.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment");
+                            httpResponseMessage.Content.Headers.ContentDisposition.FileName = filePath;
+                            httpResponseMessage.StatusCode = HttpStatusCode.OK;
+                            return httpResponseMessage;
+                        }
+                    }
+                }
+                else
+                {
+                    httpResponseMessage.StatusCode = HttpStatusCode.NotFound;
+                    return httpResponseMessage;
+                }
+            }
+            catch (Exception ex)
+
+            {
+                return this.Request.CreateResponse(HttpStatusCode.InternalServerError, ex);
+            }
+
+        }
+
+
+        [HttpPost]
+        public FolderData Info([FromBody] string FolderPath)
+        {
+            FolderData data= new FolderData(FolderPath);
+            return data;
+        }
+
+
+        private static void CheckHDListForReadiness(ref List<DriveInfo> HardDrivesList)
+        {
+            if (HardDrivesList != null)
+            {
+                HardDrivesList = DriveInfo.GetDrives().ToList();
+                try
+                {
+                    foreach (DriveInfo di in HardDrivesList)
+                    {
+                        if ((di.IsReady == false) | (di.DriveType != DriveType.Fixed))
+                        {
+                            HardDrivesList.Remove(di);
+                        }
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                }
+            }
+            else
+            {
+                Console.WriteLine("CheckHDListForReadiness: Null reference for HardDrivesList parameter was received!");
+            }
+        }
+
+    }
+}
---
+++ WebAPI/Models/FolderData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+
+namespace WebAPI.Models
+{
+    public class Entry
+    {
+        public bool DirFlag { get; set; }
+        public string Name { get; set; }
+        public long Size { get; set; }
+        public Entry(bool DirFlag, string Name, long Size)
+        {
+            this.DirFlag = DirFlag;
+            this.Name = Name;
+            this.Size = Size;
+        }
+    }
+
+        public class FolderData
+        {
+            public string CurrentPath { get; set; }
+            public List<Entry> entries;
+            private long LeftBound { get; set; }
+            private long RightBound { get; set; }
+
+            public FolderData(string targetPath, long leftBound = 10 * 1024 * 1024, long rightBound = 100 * 1024 * 1024)
+            {
+                this.CurrentPath = targetPath;
+                this.entries = new List<Entry> { };
+                this.LeftBound = leftBound;
+                this.RightBound = rightBound;
+
+                foreach (string directory in Directory.GetDirectories(targetPath))
+                {
+                    Entry entry = new Entry(true, directory.Replace(targetPath,""), 0);
+                    this.entries.Add(entry);
+                };
+
+                foreach (FileInfo file in new DirectoryInfo(targetPath).GetFiles())
+                {
+                    Entry entry = new Entry(false, file.Name, file.Length);
+                    this.entries.Add(entry);
+                    if (entry.Size < LeftBound) { SmallSizeFiles += 1; }
+                    else if (entry.Size > RightBound) { BigSizeFiles += 1; }
+                    else MiddleSizeFiles += 1;
+                }
+            }
+
+            public int SmallSizeFiles { get; set; }
+            public int MiddleSizeFiles { get; set; }
+            public int BigSizeFiles { get; set; }
+        }
+}
